d69f867244b424e04078953f7a266e79db7606bd
manjillama <lamamanjil@gmail.com>
2019-05-27 02:03:36 +0545

    new push

---
+++ build/precache-manifest.771cc2b517c791ff0a272e205357729d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a98d8435cc1a188a7797c399dc5fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ddb8d316ad2a3bd4334",
+    "url": "/static/js/2.de8d535e.chunk.js"
+  },
+  {
+    "revision": "c941359f297bb6132f40",
+    "url": "/static/js/main.ce9235db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
