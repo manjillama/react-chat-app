abf3afa8aed062d291701bef71d6fd5e7d8d6854
manjillama <lamamanjil@gmail.com>
2019-06-14 00:12:12 +0545

    updated readme and color user avatar algorithm fixed

--- src/components/chats/Sidebar.js
+++ src/components/chats/Sidebar.js
@@ -10,18 +10,13 @@ class SideBar extends Component {
     input   0 1 2 3 4 5 6 7 8 9 10 11 12
     output  0 1 2 3 4 5 0 1 2 3  4  5  0
     */
-    const i = function(index){
-      if(index >= colors.length){
-        return index - colors.length*(index/colors.length)
-      }
-      return index
-    }
+    const i = index%colors.length;
 
     const userName = user === myUsername ? user+' (You)' : user
 
     return (
       <div className="d-flex">
-        <div className="u-pp" style={{background:colors[i(index)]}}>
+        <div className="u-pp" style={{background:colors[i]}}>
           {user.charAt(0)}
         </div>
         <span style={{lineHeight:40+'px',fontSize: 13+'px'}}>{userName}</span>
